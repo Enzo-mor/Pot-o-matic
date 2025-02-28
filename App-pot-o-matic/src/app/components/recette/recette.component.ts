9b30753843ec8a66eaaf354cbed499aff47f0bd5
Pasquier Lina <s2203577@ic2s124-11.univ-lemans.fr>
2025-02-28 10:39:22 +0100

    Composant Recette,Footer,Header

---
+++ App-pot-o-matic/src/app/components/recette/recette.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recette',
+  imports: [],
+  templateUrl: './recette.component.html',
+  styleUrl: './recette.component.css'
+})
+export class RecetteComponent {
+
+}
